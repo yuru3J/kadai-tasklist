42d418043e35b0fdf993265c696680a5c4b524c5
yasuhiko.kiba <yasuhiko-kiba@x-power.co.jp>
2022-01-24 16:44:55 +0900

    tasklist NewServlet.javaの作成とIndexServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
     }
 
@@ -32,9 +32,25 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        //Taskのインスタンスを生成
+        Task t = new Task();
+
+        //tの各フィールドにデータを代入
+        String content = "test";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis()); //現在日時の取得
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+        //データベースに保存
+        em.persist(t);
+        em.getTransaction().commit();
+
+        //自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
 
         em.close();
     }
